ab8a986a1cd9778d8f70173c868d96085697839f
Jeremy Wright <wrightjmf@gmail.com>
2014-12-06 22:21:26 -0500

    Added a comment about the default introduction example.

--- CadQuery/Examples/Ex000_Introduction.py
+++ CadQuery/Examples/Ex000_Introduction.py
@@ -1,6 +1,8 @@
 # This example is meant to be used from within the CadQuery module of FreeCAD.
 # From within FreeCAD, you can make changes to this script and then click
 # CadQuery > Execute Script, or you can press F2.
+# There are more examples in the Examples directory included with this module.
+# Ex026_Lego_Brick.py is highly recommended as a great example of what CadQuery can do.
 import cadquery
 import Part
 
