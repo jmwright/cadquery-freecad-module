eb3eaad7d9e6114d71574ec3f1affec6d0196dd3
Jeremy Wright <wrightjmf@gmail.com>
2015-01-05 22:09:03 -0500

    Updated the CQ library and changed the new file template to reflect the new Helpers.show() best practice.

--- CadQuery/Templates/script_template.py
+++ CadQuery/Templates/script_template.py
@@ -1,5 +1,9 @@
 # This is a CadQuery script template
 # Add your script code below
+from Helpers import show
 
 # Length units
 length_uom = 'mm'
+
+# Use the following to render your model with grey RGB and no transparency
+# show(my_model, (204, 204, 204, 0.0))
