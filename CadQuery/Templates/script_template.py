579160f143d129ee649e5403ab2c7afa55360800
Jeremy Wright <wrightjmf@gmail.com>
2015-01-19 13:44:08 -0500

    Made some changes to the new file template.

--- CadQuery/Templates/script_template.py
+++ CadQuery/Templates/script_template.py
@@ -1,9 +1,7 @@
 # This is a CadQuery script template
 # Add your script code below
+import cadquery as cq
 from Helpers import show
-
-# Length units
-length_uom = 'mm'
 
 # Use the following to render your model with grey RGB and no transparency
 # show(my_model, (204, 204, 204, 0.0))
