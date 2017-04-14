045889b8096395975bb7406d6b1b34ef000d7411
Jeremy Mack Wright <wrightjmf@gmail.com>
2017-04-13 21:27:37 -0400

    Added example using logical operators in a string selector.

---
+++ CadQuery/Examples/Ex033_Shelled_Cube_Inside_Chamfer_With_Logical_Selector_Operators.py
@@ -0,0 +1,11 @@
+# Example using advanced logical operators in string selectors
+# to select only the inside edges on a shelled cube to chamfer. 
+import cadquery as cq
+from Helpers import show
+
+result = cq.Workplane("XY").box(2, 2, 2).\
+    faces(">Z").shell(-0.2).\
+    faces(">Z").edges("not(<X or >X or <Y or >Y)").\
+    chamfer(0.125)
+
+show(result)
