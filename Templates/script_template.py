d89b261cf71b26ee67b40ba685c229b3d19cec1d
Jeremy Mack Wright <wrightjmf@gmail.com>
2017-10-04 06:33:51 -0400

    Cleaned up a comment in the script template.

--- Templates/script_template.py
+++ Templates/script_template.py
@@ -8,4 +8,4 @@ import cadquery as cq
 
 # New method to render script results using the CadQuery Gateway Interface
 # Use the following to render your model with grey RGB and no transparency
-#show_object(result, options={"rgba":(204, 204, 204, 0.0)})
+# show_object(result, options={"rgba":(204, 204, 204, 0.0)})
