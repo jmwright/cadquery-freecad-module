8c104916399de1f4ac37540b6462f8f121adf561
Jeremy Mack Wright <wrightjmf@gmail.com>
2017-10-04 06:32:34 -0400

    Updated the script template to be CQGI-compliant.

--- Templates/script_template.py
+++ Templates/script_template.py
@@ -1,7 +1,11 @@
 # This is a CadQuery script template
 # Add your script code below
 import cadquery as cq
-from Helpers import show
 
+# The show function will still work, but is outdated now
 # Use the following to render your model with grey RGB and no transparency
 # show(my_model, (204, 204, 204, 0.0))
+
+# New method to render script results using the CadQuery Gateway Interface
+# Use the following to render your model with grey RGB and no transparency
+#show_object(result, options={"rgba":(204, 204, 204, 0.0)})
