5f5bd4d666ff1e77c6c52fbb1084faa746cd8759
Jeremy Wright <wrightjmf@gmail.com>
2021-01-05 08:19:26 -0500

    Updated script template to remove show() reference

--- Templates/script_template.py
+++ Templates/script_template.py
@@ -2,10 +2,6 @@
 # Add your script code below
 import cadquery as cq
 
-# The show function will still work, but is outdated now
-# Use the following to render your model with grey RGB and no transparency
-# show(my_model, (204, 204, 204, 0.0))
-
 # New method to render script results using the CadQuery Gateway Interface
 # Use the following to render your model with grey RGB and no transparency
 # show_object(result, options={"rgba":(204, 204, 204, 0.0)})
